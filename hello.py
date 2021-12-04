357a068dfd65452a547b188d7bd3bbf4c2bc17ad
mrk <satishkarajwad38@gmail.com>
2021-12-04 21:02:56 +0530

    python file

---
+++ hello.py
@@ -0,0 +1,17 @@
+from flask import Flask,render_template,request
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/login.html")
+def login():
+    return render_template("login.html")
+
+@app.route("/signup.html")
+def signup():
+    return render_template("signup.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
